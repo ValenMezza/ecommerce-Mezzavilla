6fa1ed97d36d67929caa652a5823bacd806d160e
Valentino Mezzavilla <valentinomezzavilla0511@gmail.com>
2022-07-28 17:42:34 -0300

    Cambios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './componets/App';
 import App from './componets/NavBar.js'
+import "../src/estilos.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
